b1548b9ca94f5e5466352c4857cb7f0a38ad7cb0
Nikita <ggift@mail.ru>
2018-10-05 16:04:24 +0500

    fix the "tinyMCE" initializing for grappelli inline fields

--- tinymce_4/static/tinymce_4/settings/full.js
+++ tinymce_4/static/tinymce_4/settings/full.js
@@ -8,28 +8,34 @@ if (typeof tinySettings === 'object') {
   if (typeof menu === 'object') {
     tinySettings.menu = menu;
   }
+
   if (typeof plugins === 'object') {
     tinySettings.plugins = plugins;
   }
+
   if (typeof externalPlugins === 'object') {
     tinySettings.external_plugins = externalPlugins;
   }
+
   if (typeof imageClassList === 'object') {
     tinySettings.image_class_list = imageClassList;
   }
+
   if (typeof languageUrl === 'string') {
     tinySettings.language_url = languageUrl
   }
+
   if (typeof tableClassList === 'object') {
     tinySettings.table_class_list = imageClassList;
   }
+
   if (typeof styleFormats === 'object') {
     tinySettings.style_formats = styleFormats.concat(customStyleFormats);
   }
+
   if (typeof templates === 'object') {
     tinySettings.templates = templates;
   }
-  tinymce.init(tinySettings);
 } else {
   console.warn('tinyMCE 4 Settings is not exists');
 }
